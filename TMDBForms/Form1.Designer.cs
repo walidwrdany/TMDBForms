b76918e6874fd4fb54ec45c9dc3fec4ee71428a5
computek246 <54823869+computek246@users.noreply.github.com>
2019-11-25 20:37:11 +0200

    Add files via upload

---
+++ TMDBForms/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TMDBForms
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.tb = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1_poster = new System.Windows.Forms.CheckBox();
+            this.checkBox1_backdrop = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // tb
+            // 
+            this.tb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb.Location = new System.Drawing.Point(12, 89);
+            this.tb.Multiline = true;
+            this.tb.Name = "tb";
+            this.tb.ReadOnly = true;
+            this.tb.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb.Size = new System.Drawing.Size(733, 612);
+            this.tb.TabIndex = 0;
+            this.tb.WordWrap = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(635, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(653, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select Path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 707);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(733, 18);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // checkBox1_poster
+            // 
+            this.checkBox1_poster.AutoSize = true;
+            this.checkBox1_poster.Location = new System.Drawing.Point(13, 21);
+            this.checkBox1_poster.Name = "checkBox1_poster";
+            this.checkBox1_poster.Size = new System.Drawing.Size(107, 17);
+            this.checkBox1_poster.TabIndex = 4;
+            this.checkBox1_poster.Text = "Download Poster";
+            this.checkBox1_poster.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1_backdrop
+            // 
+            this.checkBox1_backdrop.AutoSize = true;
+            this.checkBox1_backdrop.Location = new System.Drawing.Point(126, 21);
+            this.checkBox1_backdrop.Name = "checkBox1_backdrop";
+            this.checkBox1_backdrop.Size = new System.Drawing.Size(120, 17);
+            this.checkBox1_backdrop.TabIndex = 5;
+            this.checkBox1_backdrop.Text = "Download Backdrop";
+            this.checkBox1_backdrop.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 730);
+            this.Controls.Add(this.checkBox1_backdrop);
+            this.Controls.Add(this.checkBox1_poster);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tb);
+            this.MinimumSize = new System.Drawing.Size(650, 365);
+            this.Name = "Form1";
+            this.Text = "TMDB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1_poster;
+        private System.Windows.Forms.CheckBox checkBox1_backdrop;
+    }
+}
+
---
+++ TMDBForms/Form1.cs
@@ -0,0 +1,430 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TMDbLib.Client;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Movies;
+using TMDbLib.Objects.Search;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Net;
+using TMDbLib.Objects.Collections;
+
+namespace TMDBForms
+{
+    public partial class Form1 : Form
+    {
+
+
+        private TMDbClient client;
+        private DBVM dBVM;
+        private bool shouldSave;
+        private SearchContainer<SearchMovie> results;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new TMDbClient("c6b31d1cdad6a56a23f0c913e2482a31", true);
+            dBVM = new DBVM();
+            shouldSave = false;
+        }
+
+
+
+
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.RestoreDirectory = true;
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                textBox1.Clear();
+                tb.Clear();
+                textBox1.AppendText(dialog.FileName);
+                await Db(dialog.FileName);
+            }
+        }
+
+
+
+
+
+
+        private async Task Db(string directory)
+        {
+
+            FileInfo DB = new FileInfo("DB.json");
+            button1.Enabled = false;
+            checkBox1_poster.Enabled = false;
+            checkBox1_backdrop.Enabled = false;
+
+
+            try
+            {
+                dBVM = FetchDB(DB); //Fill Model from DataBase if Exist
+
+                await FetchConfig(client);
+
+                string[] extensions = new[] { ".MP4", ".MKV", ".TS" };
+                var listOfFiles = GetAllFilesByType(directory, extensions);
+
+
+                tb.AppendLine("");
+                tb.AppendLine("found " + listOfFiles.Count.ToString() + " movie in \"" + directory + "\"");
+                tb.AppendLine("---------------------------------------------------------------------------");
+
+                foreach (var _movie in listOfFiles)
+                {
+                    try
+                    {
+                        results = new SearchContainer<SearchMovie>();
+
+                        tb.AppendLine("Searched for movies: '" + Path.GetFileNameWithoutExtension(_movie) + "'");
+
+                        results = await GetMovieByNameAsync(_movie);
+
+
+                        if (results.Results.Count > 0)
+                        {
+                            tb.AppendText(", found " + results.TotalResults + " results in " + results.TotalPages + " pages");
+
+                            SearchMovie searchMovie = results.Results.FirstOrDefault();
+                            PrintResult(searchMovie);
+                            Movie movie = await GetMovieByIdAsync(searchMovie.Id, _movie);
+
+                            if (checkBox1_poster.Checked)
+                                await DownloadDataAsync(movie.PosterPath, client.Config.Images.PosterSizes.ElementAtOrDefault(client.Config.Images.PosterSizes.Count - 2), _movie);
+
+                            if (checkBox1_backdrop.Checked)
+                                await DownloadDataAsync(movie.BackdropPath, client.Config.Images.BackdropSizes.ElementAtOrDefault(client.Config.Images.BackdropSizes.Count - 2), _movie);
+                        }
+                        else
+                            tb.AppendText(", No results found");
+
+
+                        tb.AppendLine("");
+                        tb.AppendLine("");
+                        tb.AppendLine("-----------------------");
+                    }
+                    catch (Exception ex)
+                    {
+                        tb.AppendError(ex.Message);
+                        continue;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                tb.AppendError(ex.Message, ex.ToString());
+            }
+            finally
+            {
+                if (shouldSave)
+                {
+                    tb.AppendLine("Save Changes");
+                    string json = JsonConvert.SerializeObject(dBVM);
+                    File.WriteAllText(DB.FullName, json, Encoding.UTF8);
+                }
+
+                File.WriteAllText("Log.txt", tb.Text, Encoding.UTF8);
+                MessageBox.Show("Done.");
+
+                button1.Enabled = true;
+                checkBox1_poster.Enabled = true;
+                checkBox1_backdrop.Enabled = true;
+            }
+        }
+
+
+
+
+
+
+
+        private async Task DownloadDataAsync(string imagePath, string size, string item)
+        {
+            if (!String.IsNullOrWhiteSpace(imagePath))
+            {
+                var dir = Path.GetDirectoryName(item);
+                if (!new FileInfo(dir + imagePath).Exists)
+                {
+                    Uri imageUri = client.GetImageUrl(size, imagePath);
+                    tb.AppendLine(" " + imageUri);
+
+                    using (WebClient wc = new WebClient())
+                    {
+                        wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                        await wc.DownloadFileTaskAsync(new Uri(imageUri.ToString()), dir + imagePath);
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+        private async Task<SearchContainer<SearchMovie>> GetMovieByNameAsync(string stPath)
+        {
+
+            int year = 0;
+            string name = "";
+            string[] str2 = Regex.Replace(Path.GetFileNameWithoutExtension(stPath), @"[.()\:\!\-\[\]]", " ")
+                .Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var K = str2.Where(a => a.Length == 4 && int.TryParse(a, out int _temp)).LastOrDefault();
+            if (K != null)
+            {
+                var Q = Array.IndexOf(str2, K);
+                name = string.Join(" ", str2.Take(Q));
+                int.TryParse(str2[Q], out year);
+            }
+            else
+            {
+                for (int i = 0; i < str2.Length; i++)
+                {
+                    name = string.Join(" ", str2.Take(i + 1));
+                    results = await client.SearchMovieAsync(name);
+                    if (results.Results.Count > 0)
+                        continue;
+                    else
+                    {
+                        name = string.Join(" ", str2.Take(i));
+                        break;
+                    }
+                }
+            }
+
+            results = await client.SearchMovieAsync(name, 0, false, year);
+            results = CheckResults(results, name);
+
+            return results;
+        }
+
+
+
+
+
+        private SearchContainer<SearchMovie> CheckResults(SearchContainer<SearchMovie> results, string name)
+        {
+            if (results.Results.Count > 1)
+                results.Results = results.Results.Where(a => a.Title.Like(name)).ToList();
+            return results;
+        }
+
+
+
+
+        private void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+
+
+
+
+        private async Task<Movie> GetMovieByIdAsync(int id, string moviePath)
+        {
+            Movie movie = new Movie();
+            Location location = new Location();
+
+            if (!dBVM.Movies.Any(a => a.Id == id))
+            {
+                movie = await client.GetMovieAsync(id, MovieMethods.Credits | MovieMethods.ReleaseDates | MovieMethods.Videos | MovieMethods.Keywords);
+                dBVM.Movies.Add(movie);
+                dBVM.Locations.Add(new Location() { Id = movie.Id, MoviePath = moviePath });
+
+                shouldSave = true;
+
+                return movie;
+            }
+
+            movie = dBVM.Movies.FirstOrDefault(s => s.Id == id);
+            location = dBVM.Locations.FirstOrDefault(a => a.Id == id);
+
+            if (location.MoviePath != moviePath)
+            {
+                location.MoviePath = moviePath;
+                shouldSave = true;
+            }
+
+            return movie;
+        }
+
+
+
+
+
+
+
+        private async Task FetchConfig(TMDbClient client)
+        {
+            FileInfo configJson = new FileInfo("config.json");
+
+            tb.AppendLine("Config file: " + configJson.FullName + ", Exists: " + configJson.Exists);
+
+            if (configJson.Exists && configJson.LastWriteTimeUtc >= DateTime.UtcNow.AddHours(-1))
+            {
+                tb.AppendLine("Using stored config");
+                string json = File.ReadAllText(configJson.FullName, Encoding.UTF8);
+
+                client.SetConfig(JsonConvert.DeserializeObject<TMDbConfig>(json));
+            }
+            else
+            {
+                tb.AppendLine("Getting new config");
+                var config = await client.GetConfigAsync();
+
+                tb.AppendLine("Storing config");
+                string json = JsonConvert.SerializeObject(config);
+                File.WriteAllText(configJson.FullName, json, Encoding.UTF8);
+            }
+        }
+
+
+
+
+
+
+        private DBVM FetchDB(FileInfo DB)
+        {
+            tb.AppendLine("DB file: " + DB.FullName + ", Exists: " + DB.Exists);
+
+            dBVM.Movies = new List<Movie>();
+            dBVM.Collections = new List<Collection>();
+            dBVM.Locations = new List<Location>();
+
+            if (DB.Exists)
+            {
+                tb.AppendLine("Using stored DB");
+                string json = File.ReadAllText(DB.FullName, Encoding.UTF8);
+                dBVM = JsonConvert.DeserializeObject<DBVM>(json);
+            }
+            else
+                tb.AppendLine("Creating new DB");
+
+            return dBVM;
+        }
+
+
+
+
+        private List<string> GetAllFilesByType(string directory, string[] types)
+        {
+            return Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories)
+                .Where(file => types.Any(x => file.EndsWith(x, StringComparison.OrdinalIgnoreCase))).ToList();
+        }
+
+
+
+
+
+        private void PrintResult(SearchMovie result)
+        {
+            tb.AppendLine(" ID : " + result.Id);
+            tb.AppendLine(" Title : " + result.Title);
+            tb.AppendLine(" Original Title : " + result.OriginalTitle);
+
+            if (result.ReleaseDate.HasValue)
+                tb.AppendLine(" Release date : " + result.ReleaseDate.Value.ToString("dd-MM-yyyy"));
+
+            tb.AppendLine(" Popularity : " + result.Popularity);
+            tb.AppendLine(" Vote Average : " + result.VoteAverage);
+            tb.AppendLine(" Vote Count : " + result.VoteCount);
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    public static class MyStringExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="toSearch"></param>
+        /// <param name="toFind"></param>
+        /// <returns></returns>
+        public static bool Like(this string toSearch, string toFind)
+        {
+            return Regex.Replace(Regex.Replace(toSearch.ToLower(), @"[.()\:\!\-\[\]]", " ").Trim(), @"\s+", " ") == toFind.ToLower() ? true : false;
+        }
+    }
+
+
+
+    public static class WinFormsExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="value"></param>
+        public static void AppendLine(this TextBox source, string value)
+        {
+            if (source.Text.Length == 0)
+                source.Text = value;
+            else
+                source.AppendText(Environment.NewLine + value);
+        }
+
+
+        public static void AppendError(this TextBox textBox, string ex_Message, string ex = "")
+        {
+            textBox.AppendLine("");
+            textBox.AppendLine(ex_Message);
+            textBox.AppendLine(ex);
+            textBox.AppendLine("");
+            textBox.AppendLine("");
+            textBox.AppendLine("-----------------------");
+        }
+    }
+
+
+
+
+
+
+    public class DBVM
+    {
+        public List<Movie> Movies { get; set; }
+        public List<Collection> Collections { get; set; }
+        public List<Location> Locations { get; set; }
+    }
+
+
+
+
+
+    public class Location
+    {
+        public int Id { get; set; }
+        public string MoviePath { get; set; }
+    }
+
+
+
+
+}
