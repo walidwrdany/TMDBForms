ed16d9c9fc801fdf7378712399cd3fd9fe91a001
samarabdelfatah <samar.a.abdelfatah>
2020-01-07 21:02:06 +0200

    wq

--- TMDBForms/Program.cs
+++ TMDBForms/Program.cs
@@ -14,6 +14,7 @@ namespace TMDBForms
         [STAThread]
         static void Main()
         {
+            //waled test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
